68631cc6424748d6d73489014d71dcf41389a161
rc808129 <rc808129@gmail.com>
2025-12-29 19:08:51 +0530

    commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,24 @@
 
 import React from "react"
-const TodoList = ({todos,handleComplete})=>{
+const TodoList = ({ todos, handleComplete }) => {
+  return (
+    <div>
+      {todos.map((item) => (
+        <ul key={item.id}>
+          <li>
+            {item.item}
 
+            {!item.flag && (
+              <button onClick={() => handleComplete(item.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
 
-  return(<div>
-    {todos.map((item)=>{
-      return(<ul key={item.id}>
-         <li>{item.item}</li> 
-        
-        {!item.flag && (
-          <button onClick={() => handleComplete(item.id)}>
-            Complete
-          </button>
-        )}
-      </ul>
-
-      )
-    })}
-
-  </div>)
-}
 
 export default TodoList
